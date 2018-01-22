ccb712d64d57eb41911514e5f0fe2d2a222448e8
Known Chen <knownchen@163.com>
2018-01-22 20:39:42 +0800

    update  left menu style

--- src/components/LeftMenu.js
+++ src/components/LeftMenu.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Menu from './Menu'
 
-const LeftMenu = ({ menus }) => <Menu menus={menus} bsStyle="pills" stacked />
+const LeftMenu = ({ menus }) => <Menu menus={menus} className="left-menu" stacked />
 
 LeftMenu.propTypes = {
     menus: PropTypes.array
