cd7d87be54e881c93516fde2efda2bc94d22e889
Known Chen <knownchen@163.com>
2018-01-25 20:27:57 +0800

    update forms

--- src/routes/Basic/Forms.js
+++ src/routes/Basic/Forms.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import {
-    FormGroup, ControlLabel, FormControl, HelpBlock,
+    Form, FormGroup, ControlLabel, FormControl, HelpBlock,
+    Grid, Row, Col,
     Button, Checkbox, Radio
 } from 'react-bootstrap'
 
 const TodoList = ({ items }) => (
     <ul>
-        {items.map(item => <li key={item.id}>{item.text}</li>)}
+        {items.map(item => <li key={item.id}>{JSON.stringify(item)}</li>)}
     </ul>
 )
 
@@ -15,141 +16,146 @@ TodoList.propTypes = {
     items: PropTypes.array
 }
 
-const FieldGroup = ({ id, label, help, ...props }) => (
-    <FormGroup controlId={id}>
+const FieldGroup = ({ id, validationState, label, help, ...props }) => (
+    <FormGroup controlId={id} validationState={validationState}>
         <ControlLabel>{label}</ControlLabel>
         <FormControl {...props} />
         {help && <HelpBlock>{help}</HelpBlock>}
     </FormGroup>
 )
 
-class TodoApp extends Component {
+const AppForm = ({ onChange }) => (
+    <Grid>
+        <Row className="show-grid">
+            <Col xs={4} md={4}>
+                <FieldGroup
+                    id="Todo"
+                    type="text"
+                    label="事项"
+                    help="至少需要输入5-10个字符。"
+                    placeholder="请输入TODO"
+                    onChange={onChange}
+                />
+                <FieldGroup
+                    id="Email"
+                    type="email"
+                    label="Email address"
+                    placeholder="Enter email"
+                    onChange={onChange}
+                />
+                <FieldGroup
+                    id="File"
+                    type="file"
+                    label="File"
+                    help="Example block-level help text here."
+                    onChange={onChange}
+                />
+                <Checkbox checked readOnly>Checkbox</Checkbox>
+                <Radio checked readOnly>Radio</Radio>
+                <FormGroup controlId="Select">
+                    <ControlLabel>Select</ControlLabel>
+                    <FormControl componentClass="select" placeholder="select" onChange={onChange}>
+                        <option value="select">select</option>
+                        <option value="Banana">Banana</option>
+                        <option value="Apple">Apple</option>
+                    </FormControl>
+                </FormGroup>
+                <FormGroup controlId="SelectMultiple">
+                    <ControlLabel>Multiple select</ControlLabel>
+                    <FormControl componentClass="select" multiple onChange={onChange}>
+                        <option value="select">select (multiple)</option>
+                        <option value="Banana">Banana</option>
+                        <option value="Apple">Apple</option>
+                    </FormControl>
+                </FormGroup>
+            </Col>
+            <Col xs={4} md={4}>
+                <FieldGroup
+                    id="Text"
+                    type="text"
+                    label="Text"
+                    placeholder="Enter text"
+                    onChange={onChange}
+                />
+                <FieldGroup id="Password" label="Password" type="password" />
+                <FormGroup>
+                    <Checkbox inline>1</Checkbox> <Checkbox inline>2</Checkbox>{' '}
+                    <Checkbox inline>3</Checkbox>
+                </FormGroup>
+                <FormGroup>
+                    <Radio name="radioGroup" inline>1</Radio>{' '}
+                    <Radio name="radioGroup" inline>2</Radio>{' '}
+                    <Radio name="radioGroup" inline>3</Radio>
+                </FormGroup>
+                <FormGroup controlId="Textarea">
+                    <ControlLabel>Textarea</ControlLabel>
+                    <FormControl componentClass="textarea" placeholder="textarea" onChange={onChange} />
+                </FormGroup>
+                <FormGroup>
+                    <ControlLabel>Static text</ControlLabel>
+                    <FormControl.Static>email@example.com</FormControl.Static>
+                </FormGroup>
+            </Col>
+        </Row>
+    </Grid>
+)
+
+class App extends Component {
     state = {
         items: [],
-        isLoading: false,
-        text: ''
+        isLoading: false
     }
 
     getValidationState = () => {
-        const length = this.state.text.length;
-        if (length > 10) return 'success';
-        else if (length > 5) return 'warning';
-        else if (length > 0) return 'error';
+        //const length = this.state.text.length;
+        //if (length > 10) return 'success';
+        //else if (length > 5) return 'warning';
+        //else if (length > 0) return 'error';
         return null;
     }
 
     handleChange = (e) => {
         const target = e.target
         const value = target.type === 'checkbox' ? target.checked : target.value
-        const name = target.name
+        const name = target.id
         this.setState({ [name]: value })
     }
 
     handleSubmit = (e) => {
         e.preventDefault()
-        if (!this.state.text.length) {
-            return
-        }
-
         this.setState({ isLoading: true })
 
+        const { items, isLoading, ...state } = this.state
         const newItem = {
             id: Date.now(),
-            text: this.state.text
+            ...state
         }
 
         this.setState(prevState => ({
             items: prevState.items.concat(newItem),
-            text: '',
             isLoading: false
         }))
     }
 
     render() {
-        const { items, text, isLoading } = this.state
+        const { items, isLoading } = this.state
         return (
             <div>
                 <h3>表单</h3>
                 <TodoList items={items} />
-                <form>
-                    <FormGroup
-                        controlId="formBasicText"
-                        validationState={this.getValidationState()}
-                    >
-                        <ControlLabel>事项</ControlLabel>
-                        <FormControl
-                            type="text"
-                            value={text}
-                            placeholder="请输入TODO"
-                            onChange={this.handleChange}
-                        />
-                        <FormControl.Feedback />
-                        <HelpBlock>至少需要输入5-10个字符。</HelpBlock>
-                    </FormGroup>
-                    <FieldGroup
-                        id="formControlsText"
-                        type="text"
-                        label="Text"
-                        placeholder="Enter text"
-                        onChange={this.handleChange}
-                    />
-                    <FieldGroup
-                        id="formControlsEmail"
-                        type="email"
-                        label="Email address"
-                        placeholder="Enter email"
-                    />
-                    <FieldGroup id="formControlsPassword" label="Password" type="password" />
-                    <FieldGroup
-                        id="formControlsFile"
-                        type="file"
-                        label="File"
-                        help="Example block-level help text here."
-                    />
-                    <Checkbox checked readOnly>Checkbox</Checkbox>
-                    <Radio checked readOnly>Radio</Radio>
-                    <FormGroup>
-                        <Checkbox inline>1</Checkbox> <Checkbox inline>2</Checkbox>{' '}
-                        <Checkbox inline>3</Checkbox>
-                    </FormGroup>
-                    <FormGroup>
-                        <Radio name="radioGroup" inline>1</Radio>{' '}
-                        <Radio name="radioGroup" inline>2</Radio>{' '}
-                        <Radio name="radioGroup" inline>3</Radio>
-                    </FormGroup>
-                    <FormGroup controlId="formControlsSelect">
-                        <ControlLabel>Select</ControlLabel>
-                        <FormControl componentClass="select" placeholder="select">
-                            <option value="select">select</option>
-                            <option value="other">...</option>
-                        </FormControl>
-                    </FormGroup>
-                    <FormGroup controlId="formControlsSelectMultiple">
-                        <ControlLabel>Multiple select</ControlLabel>
-                        <FormControl componentClass="select" multiple>
-                            <option value="select">select (multiple)</option>
-                            <option value="other">...</option>
-                        </FormControl>
-                    </FormGroup>
-                    <FormGroup controlId="formControlsTextarea">
-                        <ControlLabel>Textarea</ControlLabel>
-                        <FormControl componentClass="textarea" placeholder="textarea" />
-                    </FormGroup>
-                    <FormGroup>
-                        <ControlLabel>Static text</ControlLabel>
-                        <FormControl.Static>email@example.com</FormControl.Static>
-                    </FormGroup>
+                <Form>
+                    <AppForm onChange={this.handleChange} />
                     <Button
                         bsStyle="primary"
                         disabled={isLoading}
                         onClick={!isLoading ? this.handleSubmit : null}
                     >
-                        {isLoading ? '提交中...' : 'Add #' + (items.length + 1)}
+                        {isLoading ? '提交中...' : '提交'}
                     </Button>
-                </form>
+                </Form>
             </div>
         )
     }
 }
 
-export default TodoApp
+export default App
